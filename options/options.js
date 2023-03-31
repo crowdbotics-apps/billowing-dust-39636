b7f0596e665e4926231667ca55143810771ae75b
Crowdbotics <team@crowdbotics.com>
2023-03-31 16:52:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-dust-39636",
+  url: "https://billowing-dust-39636.botics.co",
+  api: "https://billowing-dust-39636.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
