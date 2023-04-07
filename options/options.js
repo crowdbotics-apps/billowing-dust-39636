e2157f0f275edf769c7bffb51f924884f046f035
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-04-07 20:17:30 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "billowing-dust-39636",
-  url: "https://billowing-dust-39636.botics.co",
-  api: "https://billowing-dust-39636.botics.co/api/v1"
-}
+  "name": "billowing-dust-39636",
+  "url": "https://billowing-dust-39636.botics.co",
+  "api": "https://billowing-dust-39636.botics.co/api/v1",
+  "initialRoute": "signup2"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
